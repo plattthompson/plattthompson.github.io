e5ba0d10f6441810fddcf2b155f63b8bac1f61b7
Platt Thompson <plattthompsonprogramming@gmail.com>
2019-02-23 00:49:37 -0800

    changed DOM event listener, added contact styling

--- script.js
+++ script.js
@@ -2,19 +2,13 @@
 
 const contact = document.getElementById("email-appear");
 
-let nodeNumber = 1;
-let num = 0;
-
 contact.addEventListener("click", function(e) {
 	e.preventDefault();
 	const newDiv = document.createElement("div");
 	const email = document.createTextNode("plattthompsonprogramming@gmail.com");
 	newDiv.appendChild(email);
 
-	contact.insertBefore(newDiv, contact.childNodes[nodeNumber]);
+	contact.insertBefore(newDiv, contact.childNodes[2]);
 
-	const setDivClass = document.getElementsByTagName("div")[num].setAttribute("class", "email animated slideInRight");
-
-	nodeNumber++;
-	num++;
-})
+	const setDivClass = document.getElementsByTagName("div")[0].setAttribute("class", "email animated slideInRight");
+}, {once: true});
