138cbc00e15505eac7abe8ea0d92a984c03ef3db
Platt Thompson <plattthompsonprogramming@gmail.com>
2021-05-08 16:57:16 -0600

    rewrote js syntax, added icons, changed copy, cleaned up CSS

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 //DOM function to prevent HTML-crawling spambots
 
-const contact = document.querySelector("#email-appear");
+const contact = document.querySelector('#email-appear');
 
-contact.addEventListener("click", function(e) {
-	e.preventDefault();
-	const newDiv = document.createElement("div");
-	const email = document.createTextNode("plattthompsonprogramming@gmail.com");
-	newDiv.appendChild(email);
+contact.addEventListener('click', (e) => {
+	// e.preventDefault();
+	const emailDiv = document.createElement('div');
+	const emailAddress = document.createTextNode('platt@platt.codes');
+	emailDiv.appendChild(emailAddress);
+	
+	emailDiv.className = 'email animated fadeInRight';
+	contact.insertBefore(emailDiv, contact.childNodes[2]);
 
-	contact.insertBefore(newDiv, contact.childNodes[2]);
+	// const setDivClass = document.getElementsByTagName("div")[0].setAttribute("class", "email animated fadeInRight");
+	// const setDivClass = document.querySelectorAll("div")[0].setAttribute("class", "email animated fadeInRight");
+	// const setDivClass = document.querySelectorAll("div")[0].setAttribute("class", "email animated fadeInRight");
 
-	const setDivClass = document.getElementsByTagName("div")[0].setAttribute("class", "email animated slideInRight");
+	// emailDiv.addEventListener('scroll', (e) => {
+	// 	console.log(e);
+	// });
+
+	// 20-278
+	// 616-1022
+	// 1310-2015
+
+	// 115-450
+	// 860-1400
+	// 1690-2390
+
+	let offsetVal = undefined;
+
+	window.addEventListener('scroll', () => {
+		console.log(window.pageYOffset);
+		if (window.pageYOffset > 140 && window.pageYOffset <= 190) {
+			offsetVal = window.pageYOffset - 140;
+			emailDiv.style = 'left: ' + offsetVal + 'px;';
+		} else if (window.pageYOffset > 190 && window.pageYOffset < 400) {
+			emailDiv.style = 'left: 50px;';
+		} else if (window.pageYOffset >= 400 && window.pageYOffset < 450) {
+			offsetVal = 450 - window.pageYOffset;
+			emailDiv.style = 'left: ' + offsetVal + 'px;';
+		} else if (window.pageYOffset >= 420 && window.pageYOffset < 800) {
+			emailDiv.style = '';
+		} else if (window.pageYOffset > 800 && window.pageYOffset <= 930) {
+			offsetVal = window.pageYOffset - 800;
+			emailDiv.style = 'color: pink; left: ' + offsetVal + 'px;';
+		} else if (window.pageYOffset > 880 && window.pageYOffset < 1300) {
+			// 
+		} else if (window.pageYOffset > 1570 && window.pageYOffset < 2290) {
+			// emailDiv.style = 'color: orange; left: 210px;';
+			// emailDiv.style = 'color: orange; left: 210px;';
+		} else {
+			emailDiv.style = '';
+		}
+	});
+
 }, {once: true});
