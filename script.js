0b7dee62039b4b895921526bcff5693ea3783058
Platt Thompson <plattthompsonprogramming@gmail.com>
2021-05-10 13:18:23 -0600

    changed copy in bio, commented out page scroll logic for now

--- script.js
+++ script.js
@@ -27,31 +27,45 @@ contact.addEventListener('click', (e) => {
 	// 860-1400
 	// 1690-2390
 
-	let offsetVal = undefined;
+	// let offsetVal = undefined;
 
-	window.addEventListener('scroll', () => {
-		console.log(window.pageYOffset);
-		if (window.pageYOffset > 140 && window.pageYOffset <= 190) {
-			offsetVal = window.pageYOffset - 140;
-			emailDiv.style = 'left: ' + offsetVal + 'px;';
-		} else if (window.pageYOffset > 190 && window.pageYOffset < 400) {
-			emailDiv.style = 'left: 50px;';
-		} else if (window.pageYOffset >= 400 && window.pageYOffset < 450) {
-			offsetVal = 450 - window.pageYOffset;
-			emailDiv.style = 'left: ' + offsetVal + 'px;';
-		} else if (window.pageYOffset >= 420 && window.pageYOffset < 800) {
-			emailDiv.style = '';
-		} else if (window.pageYOffset > 800 && window.pageYOffset <= 930) {
-			offsetVal = window.pageYOffset - 800;
-			emailDiv.style = 'color: pink; left: ' + offsetVal + 'px;';
-		} else if (window.pageYOffset > 880 && window.pageYOffset < 1300) {
-			// 
-		} else if (window.pageYOffset > 1570 && window.pageYOffset < 2290) {
-			// emailDiv.style = 'color: orange; left: 210px;';
-			// emailDiv.style = 'color: orange; left: 210px;';
-		} else {
-			emailDiv.style = '';
-		}
-	});
+	// window.addEventListener('scroll', () => {
+
+	// 	if (window.pageYOffset > 140 && window.pageYOffset <= 190) {
+	// 		offsetVal = window.pageYOffset - 140;
+	// 		emailDiv.style = 'left: ' + (offsetVal / 16) + 'rem;';
+	// 	} else if (window.pageYOffset > 190 && window.pageYOffset < 400) {
+	// 		emailDiv.style = 'left: 3.125rem;';
+	// 	} else if (window.pageYOffset >= 400 && window.pageYOffset < 450) {
+	// 		offsetVal = 450 - window.pageYOffset;
+	// 		emailDiv.style = 'left: ' + (offsetVal / 16) + 'rem;';
+	// 	// Intro end
+	// 	} else if (window.pageYOffset >= 420 && window.pageYOffset < 820) {
+	// 		emailDiv.style = '';
+	// 	// Bio start
+	// 	} else if (window.pageYOffset > 820 && window.pageYOffset <= 900) {
+	// 		offsetVal = (window.pageYOffset - 820) + (window.pageYOffset - 820);
+	// 		emailDiv.style = 'left: ' + (offsetVal / 16) + 'rem;';
+	// 	} else if (window.pageYOffset > 900 && window.pageYOffset < 1280) {
+	// 		emailDiv.style = 'left: 10rem;';
+	// 	} else if (window.pageYOffset >= 1280 && window.pageYOffset < 1360) {
+	// 		offsetVal = (1440 - window.pageYOffset) - (window.pageYOffset - 1280);
+	// 		emailDiv.style = 'left: ' + (offsetVal / 16) + 'rem;';
+	// 	// Bio end
+	// 	} else if (window.pageYOffset >= 1360 && window.pageYOffset <= 1530) {
+	// 		emailDiv.style = '';
+	// 	// Tech start
+	// 	} else if (window.pageYOffset > 1530 && window.pageYOffset <= 1635) {
+	// 		offsetVal = (window.pageYOffset - 1530) + (window.pageYOffset - 1530);
+	// 		emailDiv.style = 'left: ' + (offsetVal / 16) + 'rem;';
+	// 	} else if (window.pageYOffset > 1635 && window.pageYOffset < 2140) {
+	// 		emailDiv.style = 'left: 13.125rem;';
+	// 	} else if (window.pageYOffset >= 2140 && window.pageYOffset < 2245) {
+	// 		offsetVal = (2350 - window.pageYOffset) - (window.pageYOffset - 2140);
+	// 		emailDiv.style = 'left: ' + (offsetVal / 16) + 'rem;';
+	// 	} else {
+	// 		emailDiv.style = '';
+	// 	}
+	// });
 
 }, {once: true});
